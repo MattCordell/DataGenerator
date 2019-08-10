59e338bdb9e78f907b3609a8b9190c00fccc4176
MattCordell <matt@snoyowie.com>
2019-08-11 00:42:33 +1000

    Generator functionally complete now

--- HealthConditionGenerator/Program.cs
+++ HealthConditionGenerator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
 
@@ -13,6 +11,15 @@ namespace HealthConditionGenerator
         static void Main(string[] args)
         {
             var HG = new HealthConditionGenerator();
+
+            var rnd = new Random();
+
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine(HG.GetHealthCondition(rnd.Next(120), true));
+                Console.WriteLine(HG.GetHealthCondition(rnd.Next(120), false));
+            }
+            
 
             Console.WriteLine("Done");
 
@@ -32,6 +39,9 @@ namespace HealthConditionGenerator
         List<Int64> availableCodes = new List<long>();
         Random rng = new Random(42);
         const string Endpoint = "https://ontoserver.csiro.au/stu3-latest";
+        List<Int64> femaleExclusiveCodes;
+        List<Int64> MaleExclusiveCodes;
+        List<Int64> fertilityCodes;
 
 
         public HealthConditionGenerator()
@@ -39,63 +49,106 @@ namespace HealthConditionGenerator
             FemaleFrequencies = InitialiseFrequencies(Properties.Resources.HealthConditions_Female);
             MaleFrequencies = InitialiseFrequencies(Properties.Resources.HealthConditions_Male);
 
-            //availableCodes = ECLexpandToList("^32570071000036102");
+            availableCodes = ECLexpandToList("^32570071000036102");
 
-            //femaleExclusiveCodes = ECLexpandToList(string ecl);
-            //maleExclusiveCodes = ECLexpandToList(string ecl);
-            //fertilityCodes = ECLexpandToList(string ecl);
+            femaleExclusiveCodes = ImportIdList(Properties.Resources.FemaleExclusiveConcepts);
+            MaleExclusiveCodes = ImportIdList(Properties.Resources.MaleExclusiveConcepts);
+            fertilityCodes = ImportIdList(Properties.Resources.FertilityConditions);
 
             ECLs = InitialiseHealthConditions(Properties.Resources.OrderedHealthConditions);
-
-
         }
 
-        public List<long> ECLexpandToList(string v)
-        {      
-            //need to paginate :(
-            const string ValueSetURL = "http://snomed.info/sct?fhir_vs=ecl/";
-            var exp = string.Concat(ValueSetURL,v);
+        private List<long> ImportIdList(string resourceFile)
+        {
+            var l = new List<long>();
 
+            var ids = resourceFile.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (var id in ids)
+            {
+                l.Add(long.Parse(id));
+            }
+            return l;
+        }
 
-            var client = new FhirClient(Endpoint);
-            var url = new FhirUri(exp);
-            var result = client.ExpandValueSet(url);
-
-            var q = result.Expansion.Contains.Select(x => x.Code);
-
+        public List<long> ECLexpandToList(string ecl)
+        {            
+            const string TerminologyEndpoint = "https://ontoserver.csiro.au/stu3-latest";
+            const string eclURL = "http://snomed.info/sct?fhir_vs=ecl/";
+            var ValueSetURL = string.Concat(eclURL, ecl);
+            var client = new FhirClient(TerminologyEndpoint);
+            int pageSize = 25000;
             var codes = new List<long>();
 
+            var parameters = new Parameters
+            {
+                Parameter = new List<Parameters.ParameterComponent>
+                {
+                new Parameters.ParameterComponent {Name = "url", Value = new FhirUri(ValueSetURL)},
+                new Parameters.ParameterComponent {Name = "count", Value = new Integer(pageSize)},
+                new Parameters.ParameterComponent {Name = "offset", Value = new Integer(0)}
+                }
+            };
+            
+            var result = (ValueSet)client.TypeOperation<ValueSet>("expand", parameters);            
+
+            // get each of the codes in the first page of results
+            var q = result.Expansion.Contains.Select(x => x.Code);
             foreach (var item in q)
             {
                 codes.Add(long.Parse(item));
             }
 
+            //if the value set is more than 1 page, iterate through and get them all!
+            for (int i = pageSize; i < result.Expansion.Total; i = i + pageSize)
+            {
+                parameters["offset"].Value = new Integer(i);
+                result = (ValueSet)client.TypeOperation<ValueSet>("expand", parameters);                
+                q = result.Expansion.Contains.Select(x => x.Code);
+
+                foreach (var item in q)
+                {
+                    codes.Add(long.Parse(item));
+                }
+            }
+
+            Console.WriteLine("Fetched " + ecl + " : " + result.Expansion.Total);
             return codes;
         }
 
         public string GetHealthCondition(int age, bool isfemale)
         {
-            //Add some fertility conditions
-            //Add sex exclusive filters
+            var candidates = new List<long>();
+            //roll dice first to try for a baby!
+            //otherwise...
 
             if (isfemale)
             {
+                //get age specific frequencies
                 var ageFrequencies = FemaleFrequencies.Where(i => i.Key >= age).FirstOrDefault().Value;
-                // Below could be out of If, test performance for linq execution
+                // choose a random frequency
                 var freq = ageFrequencies.Where(x => x >= rng.NextDouble()).FirstOrDefault();
                 var healthIndex = Array.IndexOf(ageFrequencies.ToArray(), freq);
-                return ECLs[healthIndex][rng.Next(ECLs[healthIndex].Count)].ToString();
+                
+                candidates = ECLs[healthIndex].Except(MaleExclusiveCodes).ToList();
 
             }
             else
             {
                 var ageFrequencies = MaleFrequencies.Where(i => i.Key >= age).FirstOrDefault().Value;
-                // Below could be out of If, test performance for linq execution
+                // choose a random frequency
                 var freq = ageFrequencies.Where(x => x >= rng.NextDouble()).FirstOrDefault();
                 var healthIndex = Array.IndexOf(ageFrequencies.ToArray(), freq);
-                return ECLs[healthIndex][rng.Next(ECLs[healthIndex].Count)].ToString();
+
+                candidates = ECLs[healthIndex].Except(MaleExclusiveCodes).ToList();
             }
 
+            //age based exclusions
+            if (age < 15 || age > 50)
+            {
+                candidates = candidates.Except(fertilityCodes).ToList();
+            }
+
+            return candidates[rng.Next(candidates.Count)].ToString();
         }
 
         internal Dictionary<int, List<double>> InitialiseFrequencies(string frequencyFileResource)
@@ -151,24 +204,29 @@ namespace HealthConditionGenerator
         internal List<List<Int64>> InitialiseHealthConditions(string ECLfile)
         {
             var input = ECLfile.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-            var ECL = new List<List<Int64>>();
-            var uber = new List<List<Int64>>();
+            var ecls = new List<List<Int64>>();            
 
             //interate and expand
             foreach (var line in input)
             {
                 Console.WriteLine("Expanding " + line);
+                //Execute valueSet Expand on the ECL
+                var temp = ECLexpandToList(line);
+                var og = temp.Count;
+                //Restrict to codes that haven't already been used
+                temp = temp.Intersect(availableCodes).ToList();
+                //Remove from "available codes"
+                availableCodes = availableCodes.Except(temp).ToList();
 
-                uber.Add(ECLexpandToList(line));
-                Console.WriteLine("Done Expanind");
-                //Execute valueSet Expand on the ECL (standalone method)
-                //Add each code to List<>
-                //Exclude codes that have already been used (not available)
-                //Remove remainder from "available codes"
+                Console.WriteLine("ECL {0} size {1} / {2}", line, temp.Count, og);
 
+                //just incase there's no codes left...
+                if (temp.Count == 0) { temp.Add(213257006); } // Generally unwell (finding)
+                
+                ecls.Add(temp);
             }
 
-            return ECL;
+            return ecls;
         }
 
     }
