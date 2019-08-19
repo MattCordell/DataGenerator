98e7124480a44cf5d0b1581b19d7cdd158481135
MattCordell <matt@snoyowie.com>
2019-08-19 21:36:11 +1000

    MedGenerator completed

--- MedGenerator/Program.cs
+++ MedGenerator/Program.cs
@@ -11,12 +11,33 @@ namespace MedGenerator
     {
         static void Main(string[] args)
         {
-            //initialise MedConditions
-            var MedConditions = new List<(HashSet<long> conditions, List<long> medicines)>();
 
+            var meds = new MedGenerator();
+
+            Console.WriteLine(meds.GetMedicationForCondition(103017008));
+            Console.WriteLine(meds.GetMedicationForCondition(103017008));
+            Console.WriteLine(meds.GetMedicationForCondition(103017008));
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+
+    class MedGenerator
+    {
+        public List<(HashSet<long> conditions, List<long> medicines)> MedConditions = new List<(HashSet<long> conditions, List<long> medicines)>();
+        private Random rng = new Random();
+
+        public MedGenerator()
+        {
+            //initialise MedConditions
             var fhir = new FHIRhelper.FHIRhelper();
 
+            //ConditionMedECLs
+            // first column is an ECL for a set of conditions.
+            // Second column is an ECL for associated medications (AMT ingredients) according to ATC
             string[] ConditionMedECLs = Properties.Resources.Condition_Substance_ECL_Map.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
             foreach (var item in ConditionMedECLs)
             {
                 var x = item.Split('\t');
@@ -30,11 +51,24 @@ namespace MedGenerator
 
                 MedConditions.Add((bucket, TPUUs));
             }
-
-            Console.WriteLine("Done");
-            Console.ReadKey();
-
         }
+
+        public long GetMedicationForCondition(long condition)
+        {
+            //shuffle the list to encourage variety, as condition hashes are not disjoint.
+            MedConditions = MedConditions.OrderBy(x => rng.Next()).ToList();
+
+            foreach (var item in MedConditions)
+            {
+                if (item.conditions.Contains(condition))
+                {
+                    return item.medicines.OrderBy(x => rng.Next()).FirstOrDefault();
+                }
+            }
+
+            return 0;
+        }
+
     }
 
 }
