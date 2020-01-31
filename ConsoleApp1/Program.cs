1f412c96ca1bdafc9da4eadb1abf90c6a061b580
MattCordell <matt@snoyowie.com>
2020-01-31 23:27:23 +1000

    Recommit?

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troschuetz.Random;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poiss = new Troschuetz.Random.Distributions.Discrete.PoissonDistribution(28d);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(poiss.Next().ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- TabularDataSetGenerator/Program.cs
+++ TabularDataSetGenerator/Program.cs
@@ -17,22 +17,28 @@ namespace TabularDataSetGenerator
     {
         static void Main(string[] args)
         {
-            var datasetSize = 1000000;
+            var datasetSize = 500000;
 
+            //500K-with UUID = 43.329MB
+            //500K without UUID = 25.236MB
             //10K = 439KB
             //200K = 8.7MB
             var pGenerator = new PersonGenerator.PersonGenerator(2019);
-            var patient = new Person();
-            string condition;
-            string medication = null;
             var cGenerator = new HealthConditionGenerator.HealthConditionGenerator();
             var medGenerator = new MedGenerator.MedGenerator();
+
+            var patient = new Person();
+            string condition = null;
+            string medication = null;
+            
             StringBuilder dataRow = new StringBuilder();
 
             Console.WriteLine("Start");
 
             using (TextWriter wrtr = new StreamWriter("TabularDataSetGenerator.txt"))
             {
+                Console.WriteLine("Populating file!");
+
                 for (int i = 0; i < datasetSize; i++)
                 {
                     patient = pGenerator.NewGeneralPopulationMember();
@@ -44,7 +50,7 @@ namespace TabularDataSetGenerator
                     dataRow.Append(condition);
                     dataRow.Append("\t");                    
                     dataRow.Append(medication);
-                    Console.WriteLine(dataRow);
+                    //Console.WriteLine(dataRow);
                     wrtr.WriteLine(dataRow);
                     dataRow.Clear();
                 }
